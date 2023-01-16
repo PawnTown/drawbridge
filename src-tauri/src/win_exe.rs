1253db8767c503acfc6f655e04f4202f04624b7c
Khadim Fall <khadimfall0602@gmail.com>
2023-01-16 15:01:21 +0100

    find out cec path

--- src-tauri/src/win_exe.rs
+++ src-tauri/src/win_exe.rs
@@ -2,10 +2,54 @@ use std::io::Write;
 use std::path::Path;
 use std::process::Command;
 use regex::Regex;
+use std::fs;
+use std::fs::metadata;
 
 mod asset;
 
+static NET_FRAMEWORK_PATH: &str = "C:\\Windows\\Microsoft.NET\\Framework";
 static CS_COMPILER: &str = "C:\\Windows\\Microsoft.NET\\Framework\\v4.0.30319\\csc.exe";
+
+fn get_cs_compiler_path() -> Result<std::path::PathBuf, Box<dyn std::error::Error>> {
+    let data = storage::load("settings".to_string()).unwrap();
+    let settings: Value = serde_json::from_str(&data).unwrap();
+
+    let mut custom_cs_compiler_path = settings["customCsCompilerPath"].as_str();
+    match custom_cs_compiler_path {
+        Some(path) => {
+            let path = Path::new(path);
+            if path.exists() {
+                return Ok(path.to_path_buf());
+            }
+        },
+        None => {
+            // Try to find the compiler
+            let re = Regex::new(r"\\v[\d\.]+$").unwrap();
+            let paths = fs::read_dir(NET_FRAMEWORK_PATH).unwrap().map(|r| r.unwrap()).collect();
+            paths.sort_by_key(|dir| dir.path());
+            for path in paths {
+                let path = path.unwrap().path();
+
+                let md = metadata(path).unwrap();
+                if !md.is_dir() {
+                    continue;
+                }
+
+                let path_str = path.to_str().unwrap();
+                if re.is_match(path_str) {
+                    let subpaths = fs::read_dir(path_str).unwrap();
+                    for subpath in subpaths {
+                        let subpath = subpath.unwrap().path();
+                        let subpath_str = subpath.to_str().unwrap();
+                        if subpath_str.ends_with("csc.exe") {
+                            return Ok(subpath);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
 
 fn create_cs_file(name: String, exec_path: String, id: String) -> Result<std::path::PathBuf, Box<dyn std::error::Error>> {
     let re = Regex::new(r"([\\])").unwrap();
@@ -98,7 +142,7 @@ pub fn compile_cs_file(exec_path: String, id: String, output: String) -> Result<
 
     let tmp = create_cs_file("shortcut".to_owned(), exec_path, id)?;
     let tmp  = tmp.to_str().unwrap_or("");
-    let _res = Command::new(CS_COMPILER)
+    let _res = Command::new(get_cs_compiler_path().unwrap())
         .args(&[&out, &icon, tmp])
         .current_dir(&output_path)
         .status();
